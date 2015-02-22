a04c3f195f366b17dd93563f8f0747719fafdb02
Greg Poole <gregory.poole@sydney.edu.au>
2015-02-23 00:54:52 +1100

    Player rotates to face the direction they're thrusting in

--- Assets/SalvageShip.cs
+++ Assets/SalvageShip.cs
@@ -20,15 +20,24 @@ public class SalvageShip : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			Vector3 projectedMousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-			thrust = new Vector2(projectedMousePoint.x, projectedMousePoint.y) - rigidbody2D.position;
+			thrust = (Vector2)projectedMousePoint - rigidbody2D.position;
 			thrust.Normalize();
-			thrust.x *= 2f;
-			thrust.y *= 2f;
+			thrust *= 2;
 			thrusting = true;
 		}
 
 		if (Input.GetMouseButtonUp (0)) {
 			thrusting = false;
+		}
+	}
+
+	void FixedUpdate() {
+		if (thrusting) {
+			float angle = Vector2.Angle (Vector2.up, thrust);
+			if (Vector3.Cross (Vector2.up, thrust).z < 0) {
+				angle = 360 - angle;
+			}
+			rigidbody2D.MoveRotation (angle);
 		}
 	}
 
