38a759734d3a25eab6653aa93bf2229da002f9ea
Greg Poole <gregory.poole@sydney.edu.au>
2015-02-22 22:42:45 +1100

    Version 1: fully functional

---
+++ Assets/SalvageShip.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SalvageShip : MonoBehaviour {
+
+	protected Vector2 thrust;
+
+	protected bool thrusting;
+	
+	void Start () {
+
+	}
+
+	void Update () {
+		if (thrusting) {
+			rigidbody2D.AddForce (thrust);
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 projectedMousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			thrust = new Vector2(projectedMousePoint.x, projectedMousePoint.y) - rigidbody2D.position;
+			thrust.Normalize();
+			thrust.x *= 2f;
+			thrust.y *= 2f;
+			thrusting = true;
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			thrusting = false;
+		}
+	}
+
+}
