5c42576668e3c52ee4b5d0245ca0dccabaff4d16
Greg Poole <gregory.poole@sydney.edu.au>
2015-02-23 01:12:04 +1100

    Calculate thrust magnitude based on distance of mouse from ship. Added comments.

--- Assets/SalvageShip.cs
+++ Assets/SalvageShip.cs
@@ -17,15 +17,18 @@ public class SalvageShip : MonoBehaviour {
 			rigidbody2D.AddForce (thrust);
 		}
 
+		// Press left mouse button
 		if (Input.GetMouseButtonDown (0)) {
+			// Calculate where the mouse was clicked in the game world
 			Vector3 projectedMousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-			thrust = (Vector2)projectedMousePoint - rigidbody2D.position;
-			thrust.Normalize();
-			thrust *= 2;
+			// Produce a vector that points in the direction of the mouse relative to the ship, with the magnitude
+			// based on how far away the mouse is (up to 2 units)
+			thrust = Vector2.ClampMagnitude((Vector2)projectedMousePoint - rigidbody2D.position, 2f);
 			thrusting = true;
 		}
 
+		// Stop thrusting when left button is released
 		if (Input.GetMouseButtonUp (0)) {
 			thrusting = false;
 		}
@@ -33,6 +36,9 @@ public class SalvageShip : MonoBehaviour {
 
 	void FixedUpdate() {
 		if (thrusting) {
+			// Figure out the direction of the thrust vector by calculating the angle between it and a vector pointing
+			// straight up. This calculation only works up to 180 degrees, so we have to figure out which direction the thrust vector is pointing
+			// relative to the "up" vector by using the cross product.
 			float angle = Vector2.Angle (Vector2.up, thrust);
 			if (Vector3.Cross (Vector2.up, thrust).z < 0) {
 				angle = 360 - angle;
